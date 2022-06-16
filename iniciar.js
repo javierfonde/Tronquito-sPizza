1a8e4e81a74934171f93f3273e5c943c0fbcd433
javierfonde <74280718+javierfonde@users.noreply.github.com>
2022-06-16 20:36:08 -0300

    Responsive

--- iniciar.js
+++ iniciar.js
@@ -1,9 +1,7 @@
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-  document.getElementById("mainMyForm").style.display="block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
-  document.getElementById("mainMyForm").style.display="none";
 }
