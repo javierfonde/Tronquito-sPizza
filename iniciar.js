554052cc8ea34982540ca318a6e7b3984c32dd3e
javierfonde <74280718+javierfonde@users.noreply.github.com>
2022-06-16 20:59:21 -0300

    as

--- iniciar.js
+++ iniciar.js
@@ -1,7 +1,18 @@
+window.onload= captar();
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+
+function captar(){
+  const botonIniciar = document.getElementsByClassName("textoInicio");
+  botonIniciar.onclick = iniciarSesion();
+
+}
+
+function iniciarSesion(){
+  alert("Sesion iniciada con exito!");
 }
